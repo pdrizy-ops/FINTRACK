354499ecd4b83b70b606bb9321d09ecb31ae862d
pdrizy-ops <pdrizy@gmail.com>
2026-07-17 03:21:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'fintrack-cache-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetchPromise = fetch(e.request)
+        .then((networkResp) => {
+          if (networkResp && networkResp.status === 200 && e.request.url.startsWith('http')) {
+            caches.open(CACHE_NAME).then((cache) => cache.put(e.request, networkResp.clone()));
+          }
+          return networkResp;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
