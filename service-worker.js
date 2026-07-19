d0e0b51bcc19536d1bcaca99dbdb7b14aa5772d6
pdrizy-ops <pdrizy@gmail.com>
2026-07-19 23:02:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trika-cache-v3';
+const CACHE_NAME = 'trika-cache-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
